2937cdd0a8fdbf9617c1097b31b156570cb92d04
annasyaas <annasyasinthaa@gmail.com>
2022-12-11 17:39:20 +0800

    first commit all project

---
+++ app/Http/Controllers/SimilarityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Similarity;
+use Illuminate\Http\Request;
+
+class SimilarityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Similarity  $similarity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Similarity $similarity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Similarity  $similarity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Similarity $similarity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Similarity  $similarity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Similarity $similarity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Similarity  $similarity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Similarity $similarity)
+    {
+        //
+    }
+}
